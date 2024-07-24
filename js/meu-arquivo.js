56b7a35567df550731b20b361252381b891ff034
KaioQuaresmadSouza <kaioquaresmadesouza1@gmail.com>
2024-07-23 21:48:19 -0300

    adição

--- js/meu-arquivo.js
+++
@@ -1,7 +0,0 @@
-function initMap() {
-    // Crie um objeto de mapa e especifique o elemento DOM para exibi-lo.
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -23.5505, lng: -46.6333}, // Latitude e longitude do centro do mapa (São Paulo, por exemplo)
-        zoom: 8 // Nível de zoom
-    });
-}
